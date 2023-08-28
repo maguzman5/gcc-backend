999512826097ff79307d6670cc5c346759b5ce74
maguzman5 <maguzman5@uc.cl>
2023-08-27 22:50:59 -0400

    specification of data models

--- gcc_historic_data/gcc_historic_data_db/models.py
+++ gcc_historic_data/gcc_historic_data_db/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Department(models.Model):
+    department = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = '"employees_data"."departments"'
+
+
+class Job(models.Model):
+    job = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = '"employees_data"."jobs"'
+
+
+class HiredEmployee(models.Model):
+    name = models.CharField(max_length=100)
+    datetime = models.DateTimeField(default=timezone.now)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = '"employees_data"."hired_employees"'
