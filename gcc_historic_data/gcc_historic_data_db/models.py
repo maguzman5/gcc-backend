3245264478762ed1470ecacfcdac89503b332de5
maguzman5 <maguzman5@uc.cl>
2023-08-31 10:58:25 -0400

    changing schema to add run

--- gcc_historic_data/gcc_historic_data_db/models.py
+++ gcc_historic_data/gcc_historic_data_db/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 from django.utils import timezone
 
+class Run(models.Model):
+    run_id = models.CharField(
+         primary_key = True,
+         max_length=200)
+    process = models.CharField(max_length=100)
+    timestamp = models.DateTimeField()
 
+    class Meta:
+        db_table = '"employees_data"."runs"'
+
+# Final schema
 class Department(models.Model):
     department = models.CharField(max_length=100)
+    run = models.CharField(max_length=200)
 
     class Meta:
         db_table = '"employees_data"."departments"'
@@ -11,6 +22,7 @@ class Department(models.Model):
 
 class Job(models.Model):
     job = models.CharField(max_length=100)
+    run = models.CharField(max_length=200)
 
     class Meta:
         db_table = '"employees_data"."jobs"'
@@ -21,6 +33,7 @@ class HiredEmployee(models.Model):
     datetime = models.DateTimeField(default=timezone.now)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    run = models.CharField(max_length=200)
 
     class Meta:
         db_table = '"employees_data"."hired_employees"'
